1e80bf2e31d0e3cad2b169ee5dd6505bb24ba127
ZarmeenFarooq05 <zfarooq2005@gmail.com>
2025-02-08 19:28:56 +0500

    2 commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <Home />
       <About />
       <Work />
+      <Testimonial />
       <Contact />
+      <Footer />
+
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import Logo from "../Assets/Logo.svg";
 import { BsTwitter } from "react-icons/bs";
-import { SiLinkedIn } from "react-icons/si";
+import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
     return(
-        <div>hello</div>
+        <div className='footer-wrapper'>
+            <div className='footer-section-one'>
+                <div className="footer-logo-container">
+                    <img src={Logo} />
+                </div>
+
+                <div className='footer-icons'>
+                    <BsTwitter />
+                    <SiLinkedin />
+                    <BsYoutube />
+                    <FaFacebook />
+                </div>
+            </div>
+            <div className='footer-section-coloumns'>
+                <span>Quality</span>
+                <span>Help</span>
+                <span>Carrers</span>
+                <span>Testimonials</span>
+                <span>Work</span>
+            </div>
+            <div className='footer-section-columns'>
+                <span>233-6544-8893</span>
+                <span>what@gmail.com</span>
+                <span>press@food.com</span>
+                <span>contact@food.com</span>
+            </div>
+            <div className='footer-section-columns'>
+                <span>Terms & Conditions</span>
+                <span>Privacy Policy</span>
+            </div>
+        </div>
     );
 }
+
+export default Footer;
